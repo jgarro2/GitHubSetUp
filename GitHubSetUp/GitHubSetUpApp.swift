4634d32971d7d0d7e7c7a120f5ef15dbd6a6159a
JG <coding@ndsworks.com>
2024-05-15 14:10:44 -0500

    Initial Commit

---
+++ GitHubSetUp/GitHubSetUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSetUpApp.swift
+//  GitHubSetUp
+//
+//  Created by JORGE GARROELIZONDO on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSetUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
